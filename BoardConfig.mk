c86bfbeda3b1e1ec28a3f05a8622753289adb846
kleidione Freitas <kleidione@gmail.com>
2022-03-26 08:17:24 -0300

    berlin: Drop custom UI target recovery
    Signed-off-by: kleidione <kleidione@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,9 +52,6 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/devices/platform/soc/a94000.i2c/i2c-2/2-0049/do
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# Recovery
-TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
-
 # Security
 VENDOR_SECURITY_PATCH := 2022-09-01
 
